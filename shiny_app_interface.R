20819ed4538eadf5c5daaec67a9e30e1924df46b
Skander K <44209351+skanderkam@users.noreply.github.com>
2018-12-09 15:10:09 +0100

    Added Reactives and Event Reactives for Automation
    (Still work to do)

--- shiny_app_interface.R
+++ shiny_app_interface.R
@@ -1,12 +1,40 @@
 
+# Still to do:
+# - Affiner les sliders input en éliminant les NAs et valeurs incongrues
+# - Missing value dans les coordonneés
+# - "No Preference" ne marche pas vriament encore
+# - On a supprimée l'année de fabrication du dataset ?
+
 # Please note that this is a first version whose only aim is to show what the interface could look like. 
 # No automation has been made yet regarding the input made by the user
 # We have chosen to work with the first raw dataset as a first approach, and then extend it to the entire dataset when ready
 # Name of variables as well as other choices will have to be fitted to the "clean" dataset
 
+### PACKAGES AND DATASETS AREA
+
 library(shiny)
 library(dplyr)
 library(RColorBrewer)
+library(readr)
+library(sf)
+library(ggplot2)
+library(raster)
+library(rasterVis)
+
+# For the time being, we work with dataset1
+dataset1 <- read_csv("datasets/first_dataset_full.csv")
+
+# Loading "Mapping_villes" csv file for Postal Codes matches
+mapping_villes <- read_csv2("datasets/mapping_villes.csv")
+
+# Loading the department shapes
+dept_193 <- st_read("cartography/DEPARTEMENT.shp")
+
+# Transforming the department map into region map
+region_map <- dept_193 %>% 
+  group_by(CODE_REG, NOM_REG) %>%
+  summarize()
+
 
 # Define UI for application
 ui <- fluidPage(
@@ -16,14 +44,11 @@ ui <- fluidPage(
    
    sidebarLayout(
       sidebarPanel(
-        # Slider Input for the Price
-        sliderInput("Price", "Choose a price range (in euros):", min = 1000, max = 40000, value = c(2000, 10000)),
-        
-        # Select Input for the Postal Code: for the time being, choices are linked to the "Mapping_villes" csv file
-        selectInput("postalcode", "Select your postal code:", choices = mapping_villes$codes_postaux %>% sort()),
+        # Select Input for the Postal Code:
+        selectInput("city", "Select your city:", choices = dataset1$nom_commune %>% unique %>% sort()),
         
         # Select type of car: for the time being, choices are linked to factors taken by Carosserie in the first raw dataset
-        selectInput("carosserie", "Choose the type of car:", choices = dataset1$Carrosserie %>% unique()),
+        selectInput("carrosserie", "Choose the type of car:", choices = dataset1$carrosserie %>% unique()),
         
         # Action Button (to be automatized)
         actionButton("go", "Go")
@@ -39,13 +64,13 @@ ui <- fluidPage(
         tabPanel("Complementary Adjustments",
                  # Left column with adjustment inputs
                  column(6, 
-                        selectInput("transmission", "Choose the type of transmission:", choices = c("Manual", "Automatic", "No Preference")),
+                        selectInput("transmission", "Choose the type of transmission:", choices = c("No Preference", dataset1$transmission)),
                         selectInput("brand", "Brand:", choices = c("No Preference", dataset1$brand %>% unique())),
-                        sliderInput("year_built", "Year:", min = 1950, max = 2017, value = c(2000,2014)),
-                        sliderInput("mileage", "Mileage:", min = 10000, max = 100000, value = c(50000, 80000)),
-                        selectInput("fuel", "Type of fuel:", choices = dataset1$Energie %>% unique()),
-                        selectInput("nb_seats", "Number of seats:", choices = dataset1$`Nombre de places` %>% unique()),
-                        selectInput("nb_doors", "Number of doors:", choices = dataset1$`Nb de portes` %>% unique())
+                        sliderInput("year_built", "Year:", min = 1950, max = 2017, value = c(1950,2017)),
+                        sliderInput("mileage", "Mileage:", min = 10000, max = 100000, value = c(10000, 100000)),
+                        selectInput("fuel", "Type of fuel:", choices = c("No Preference", dataset1$energie %>% unique())),
+                        selectInput("nb_seats", "Number of seats:", choices = c("No Preference", dataset1$nb_places %>% unique())),
+                        selectInput("nb_doors", "Number of doors:", choices = c("No Preference", dataset1$nb_portes %>% unique()))
                         ),
                  # Right column with impact on the price
                  column(6, 
@@ -59,9 +84,12 @@ ui <- fluidPage(
                           h3(" "),
                           h4("In the whole country"),
                           h4(" "),
-                            h5("Mean price:"),
+                            h5("Mean price: "),
+                            textOutput(outputId = "mean"),
                             h5("Minimum price:"),
-                            h5("Maximum price:")),
+                            textOutput(outputId = "min"),
+                            h5("Maximum price:"),
+                            textOutput(outputId = "max")),
                  # Bottom right column with additional informations
                  column(6,
                         h3("For your information"),
@@ -98,28 +126,6 @@ ui <- fluidPage(
 )
 
 server <- function(input, output) {
-
-  ### PACKAGES AND DATASETS AREA
-  
-  library(readr)
-  library(sf)
-  library(ggplot2)
-  library(raster)
-  library(rasterVis)
-  
-  # For the time being, we work with dataset1
-  dataset1 <- read_csv("datasets/scrapped_listings_1_1_500.csv")
-  
-  # Loading "Mapping_villes" csv file for Postal Codes matches
-  mapping_villes <- read_csv2("datasets/mapping_villes.csv")
-  
-  # Loading the department shapes
-  dept_193 <- st_read("cartography/DEPARTEMENT.shp")
-  
-  # Transforming the department map into region map
-  region_map <- dept_193 %>% 
-    group_by(CODE_REG, NOM_REG) %>%
-    summarize()
   
   # Creating centroids of each region to plot the labels at the center of each region
   centroids <- region_map %>% st_centroid()
@@ -131,29 +137,58 @@ server <- function(input, output) {
   
   # Defining location (to be replaced by automated location)
   point <- region_map %>% 
-    filter(CODE_REG == 11) %>% # Example of Ile de France is taken here as the location of the user
+    filter(CODE_REG == 53) %>% # Example of Ile de France is taken here as the location of the user
     st_centroid()
-    
+  
+  dataset1_sf <- st_as_sf(dataset1 %>% filter(!is.na(longitude) & !is.na(latitude)), 
+                          coords = c("longitude", "latitude"), crs = 4326)
+  
+  mapping_villes_sf <- st_as_sf(mapping_villes %>% filter(!is.na(longitude) & !is.na(latitude)), 
+                                 coords = c("latitude", "longitude"), crs = 4326)
+  
   # Creating an appropriate palette for the plot
   cc <- scales::seq_gradient_pal("white","orange")(seq(0,1,length.out=15))
+  
+  ### DATA INPUT AREA
+  
+  # Creating an eventreactive for the basic filters on the condition of the "Go" button
+  data_filtered_basic_country <- eventReactive(input$go, {
+    dataset1 %>%
+      filter(
+        !is.na(prix_euros),
+        carrosserie == input$carrosserie
+      )
+  })
+  
+  # Creating a reactive for the advanced filters
+  data_filtered_advanced_country <- reactive({
+    data_filtered_basic_country() %>%
+      filter(
+        transmission == ifelse(input$transmission != "No Preference", input$transmission, transmission),
+        brand == ifelse(input$brand != "No Preference", input$brand, brand),
+        kilometrage_km >= input$mileage[1] & kilometrage_km <= input$mileage[2],
+        energie == ifelse(input$fuel != "No Preference", input$fuel, energie),
+        nb_places == ifelse(input$nb_seats != "No Preference", input$nb_seats, nb_places),
+        nb_portes == ifelse(input$nb_doors != "No Preference", input$nb_doors, nb_portes)
+        )
+  })
   
   ### FUNCTION AREA
   
   # Creating a polygon of the area around the location of the user
   area_around <- function(geometry) {
-    st_buffer(geometry, dist = units::set_units(200, km)) # For the moment we have put 200 km as an example
+    st_buffer(geometry, dist = units::set_units(200, km)) %>% st_transform(crs = st_crs(region_map)) # For the moment we have put 200 km as an example
   }
   
   # Creating the polygon of the intersection of our national map and of the area around the location of the user
-  intersection <- st_intersection(region_map, area_around(point)) 
+  intersection <- st_intersection(region_map, area_around(point))
   
   # Creating a dummy dataset to have an example of plotting the results (this is just the centroid of each department)
   example_points <- dept_193 %>% 
     st_centroid() %>% 
     st_intersection(intersection)
   
-  
-  # Creating a function to plot the map of the 30 min drive area
+  # Creating a function to plot the map of the 200km area
   plot_drive_map <- function() {
     ggplot(intersection) +
       geom_sf(aes(fill = CODE_REG)) +
@@ -216,12 +251,21 @@ server <- function(input, output) {
   result_table <- function() {
     head(
       dataset1 %>% 
-      dplyr::select(Prix, brand, modele, address_pro, Kilométrage, Transmission) %>% 
-      filter(!is.na(brand) & !is.na(address_pro) & !is.na(brand))
+      dplyr::select(prix_euros, brand, modele, nom_commune, kilometrage_km, transmission) %>% 
+      filter(!is.na(brand) & !is.na(nom_commune))
     )
   }
   
-  
+  # Creating a function to give the mean, minimum and maximum price depending on the filters
+  result_price <- function() {
+          a <- c(
+            mean(data_filtered_advanced_country()$prix_euros),
+            max(data_filtered_advanced_country()$prix_euros),
+            min(data_filtered_advanced_country()$prix_euros)
+          )
+    return(a)
+  }
+
   ### OUTPUT AREA
   
    output$map <- renderPlot({
@@ -238,6 +282,18 @@ server <- function(input, output) {
    
    output$table_drive <- DT::renderDT({
      result_table()
+   })
+   
+   output$mean <- renderText({
+     round(result_price()[1])
+  })
+   
+   output$max <- renderText({
+     round(result_price()[2])
+   })
+   
+   output$min <- renderText({
+     round(result_price()[3])
    })
    
    output$fyi <- renderPlot({
