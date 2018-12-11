76bd98392ea6c5a8b577929dadd7a26b1f6f268a
ludmilaexbrayat <ludmilaexbrayat@users.noreply.github.com>
2018-12-11 19:51:34 +0100

    Automated the second map

--- shiny_app_interface.R
+++ shiny_app_interface.R
@@ -34,12 +34,22 @@ dept_193 <- st_read("cartography/DEPARTEMENT.shp")
 crs_lambert <- 2154
 
 dpt_map <- dept_193 %>% 
-  st_transform(crs = crs_lambert) #%>%                     # With the good CRS
-#st_simplify(preserveTopology = TRUE, dTolerance = 2000) # Simplifying the raster so that the map loads faster
+  st_transform(crs = crs_lambert)
 
 region_map <- dpt_map %>% 
-  group_by(NOM_REG) %>% 
+  group_by(NOM_REG, CODE_REG) %>% 
   summarize()
+
+# Adding the coordinates of the centroids of each region
+coord <- region_map %>% 
+  st_centroid() %>% 
+  arrange(CODE_REG) %>% 
+  st_coordinates() %>% 
+  as.data.frame()
+
+list_regions <- region_map %>% arrange(CODE_REG)
+
+coord$CODE_REG <- list_regions$CODE_REG
 
 
 # Define UI for application
@@ -136,30 +146,19 @@ ui <- fluidPage(
 
 server <- function(input, output) {
   
-  # Creating centroids of each region to plot the labels at the center of each region
-  centroids <- region_map %>% st_centroid()
+  ### BASE OBJECTS
   
-  # Transforming st format into coordinates format
-  coord <- centroids %>% st_coordinates() 
-  centroids <- cbind(centroids, coord) %>% 
-    mutate(price_ex = "3,338???") # This is just a dummy value for the moment
-  
-  # Defining location (to be replaced by automated location)
-  dataset1_sf <- st_as_sf(dataset1 %>% filter(!is.na(longitude) & !is.na(latitude)), 
-                          coords = c("longitude", "latitude"), crs = 4326)
-  
-  # Preparing the objects we need for the plot
+  # Preparing the dataset we'll need for the plots
   dataset_map <- dataset1 %>% 
     filter(!is.na(longitude) & !is.na(latitude)) %>% 
     st_as_sf(coords = c("longitude", "latitude"),
              crs = 4326) %>% 
     st_transform(crs = crs_lambert)
   
-  mapping_villes_sf <- st_as_sf(mapping_villes %>% filter(!is.na(longitude) & !is.na(latitude)), 
-                                coords = c("latitude", "longitude"), crs = 4326)
-  
-  # Creating an appropriate palette for the plot
+  # Creating appropriate palettes for the plot
   cc <- scales::seq_gradient_pal("white","orange")(seq(0,1,length.out=15))
+  cc_dark <- scales::seq_gradient_pal("orange","black")(seq(0,1,length.out=15))
+  
   
   ### DATA INPUT AREA
   
@@ -189,11 +188,6 @@ server <- function(input, output) {
       )
   })
   
-  # Finding the localization of the user
-  point_user <- eventReactive(input$go, {dataset_map %>%
-      filter(nom_commune == input$city) %>%
-      head(n = 1) %>%
-      st_simplify(preserveTopology = TRUE, dTolerance = 2000)})  # Simplifying the raster so that the map loads faster
   
   # Creating a reactive for the advanced filters at the 100km area level
   data_filtered_advanced_100km <- reactive({
@@ -215,22 +209,28 @@ server <- function(input, output) {
   })
   
   
-  # Filtering for points around the user input location
+  # Finding the localization of the user
+  point_user <- eventReactive(input$go, {dataset_map %>%
+      filter(nom_commune == input$city) %>%
+      head(n = 1)
+    })
+  
+  
+  # Finding the cars around the localization of the user
   points_around <- reactive({
     data_filtered_advanced_100km() %>% 
       filter(st_distance(data_filtered_advanced_100km(), point_user()) <= units::set_units(100, km))
-  })
+    })
+  
   
   # Cropping the map of France to focus on the region
   area_around <- function(geometry) {
     st_buffer(geometry, dist = units::set_units(100, km)) %>% 
-      st_transform(crs = st_crs(dpt_map)) # For the moment we have put 200 km as an example
-  }
+      st_transform(crs = st_crs(dpt_map))
+    }
   
   intersection <- reactive({
-    
     st_intersection(dpt_map, area_around(point_user()))
-    
   }) 
   
   
@@ -239,7 +239,7 @@ server <- function(input, output) {
     ggplot(intersection) +
       geom_sf(aes(fill = CODE_REG)) +
       geom_sf(data = points_around, color = "black", size = 1) +
-      geom_sf(data = point_user, color = "orange", size = 5) + # Plotting a point at the position of the user (to be automatized)
+      geom_sf(data = point_user, color = "orange", size = 5) + # Plotting a point at the position of the user
       coord_sf(crs = st_crs(dpt_map)) +
       scale_fill_manual(values = cc) + # Applying the palette we have built further up
       # All the lines below have as sole purpose to erase all the grids, axis, etc. of the plot
@@ -257,21 +257,31 @@ server <- function(input, output) {
             panel.grid.major = element_line(colour = "white"))
   }
   
-  # Creating a function to plot the map of the whole country
-  # map_prices_per_region <- eventReactive(input$go, {
-  #   dataset_map %>% 
-  #     filter(carrosserie == input$carrosserie) %>% 
-  #     group_by(nom_region) %>% 
-  #     summarize(mean_price = mean(prix_euros))
-  # })
   
+  # Computing the average price per region
+  prices_per_region <- eventReactive(input$go, {dataset_map %>% 
+    st_join(dpt_map) %>%
+    filter(carrosserie == input$carrosserie,
+           (transmission %in% input$transmission) | (input$transmission == "No Preference"),
+           (brand %in% input$brand) | (input$brand == "No Preference"),
+           year >= input$year_built[1] & year <= input$year_built[2],
+           kilometrage_km >= input$mileage[1] & kilometrage_km <= input$mileage[2],
+           (energie %in% input$fuel) | (input$fuel == "No Preference"),
+           (nb_places %in% input$nb_seats) | (input$nb_seats == "No Preference"),
+           (nb_portes %in% input$nb_doors) | (input$nb_doors == "No Preference")) %>%
+    group_by(CODE_REG) %>% 
+    summarize(mean_price = as.integer(mean(prix_euros))) %>%
+    merge(coord, by = c("CODE_REG" = "CODE_REG"))})
+  
+  
+  # Creating a function to plot the map of the whole country
   plot_map <- function(map, point_user) {
-    ggplot(map) +
-      geom_sf(aes(fill = NOM_REG)) +
-      geom_text(data = centroids, aes(x=X, y=Y, label= price_ex)) + # Adding label with prices
-      scale_fill_manual(values = cc) + # Applying the palette we have built further up
-      geom_sf(data = point_user, color = "orange", size = 5) + # Plotting a point at the position of the user (to be automatized)
-      coord_sf(crs = st_crs(map)) +
+    ggplot(region_map) +
+      geom_sf(fill = "white") +
+      geom_sf(data = point_user, color = "orange", size = 5) + # Plotting a point at the position of the user
+      geom_text(data = map, aes(x = X, y = Y, label = mean_price, colour = as.factor(mean_price))) + # Adding label with prices
+      scale_color_manual(values = cc_dark) + # Applying the palette we have built further up
+      coord_sf(crs = st_crs(region_map)) +
       #All the lines below have as sole purpose to erase all the grids, axis, etc. of the plot
       theme(axis.line=element_blank(),
             axis.text.x=element_blank(),
@@ -287,11 +297,13 @@ server <- function(input, output) {
             plot.background=element_blank())
   }
   
+  
   # Creating a dummy dataframe just for plotting the importance of parameters
   df <- data.frame(c("Number of seats", "Number of seats",
                      "Brand", "Brand", "Brand", "Brand", "Brand",
                      "Type of Fuel",
                      "Year"))
+  
   
   # Creating a function to plot the graph of importance of each parameter
   plot_fyi <- function() {
@@ -299,6 +311,7 @@ server <- function(input, output) {
       geom_bar(aes(df[,1]), fill = "orange") +
       labs(x = "Parameter", y = "Relative Importance")
   }
+  
   
   # Creating a function to draw the appropriate table 
   result_table <- function() {
@@ -311,6 +324,7 @@ server <- function(input, output) {
     return(table)
   }
   
+  
   # Creating a function to draw the appropriate table within the 100km area
   result_table_area <- function() {
     table <- as.data.frame(data_filtered_advanced_100km()) %>% 
@@ -321,6 +335,7 @@ server <- function(input, output) {
     
     return(table)
   }
+  
   
   # Creating a function to give the mean, minimum and maximum price depending on the filters
   result_price <- function() {
@@ -341,10 +356,11 @@ server <- function(input, output) {
     return(a)
   }
   
+  
   ### OUTPUT AREA
   
   output$map <- renderPlot({
-    plot_map(map = region_map, point_user = point_user())
+    plot_map(map = prices_per_region(), point_user = point_user())
   })
   
   output$drive_map <- renderPlot({
