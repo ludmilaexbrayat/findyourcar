295c32640d9532b6b505148821ab67effc95c37d
Gaelle Guillou <gaelle.m.guillou@gmail.com>
2018-12-14 15:25:43 +0100

    vignettes schmilblik

---
+++ package/findyourdreamcar/vignettes/findyourdreamcar.R
@@ -0,0 +1,13 @@
+## ----cars, warning=F-----------------------------------------------------
+#devtools::install_github("ludmilaexbrayat/findyourcar/package/findyourdreamcar")
+
+library(findyourdreamcar)
+
+# By loading the library, the cardata dataset is loaded - feel free to explore it!
+
+head(cardata, 1)
+
+# Launch the Shiny app - feel free to interact with it!
+
+# run_app()
+
