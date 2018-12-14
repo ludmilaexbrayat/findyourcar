9eed1fcd21439ecf796b72de55ea7c378d92a184
Skander K <44209351+skanderkam@users.noreply.github.com>
2018-12-14 13:50:50 +0100

    Changed regression module

--- package/findyourdreamcar/R/mod_regressionUI.R
+++ package/findyourdreamcar/R/mod_regressionUI.R
@@ -71,7 +71,7 @@ mod_regression <- function(input, output, session, dataframe) {
 
   regression_function <- function(data_reg, choix){
 
-    data_reg <- data_reg %>% filter(carrosserie == choix) #Filter the dataset for this type of car
+    data_reg <- data_reg %>% dplyr::filter(carrosserie == choix) #Filter the dataset for this type of car
 
     reg_sol <- lm(prix_euros  ~ energie + transmission+is_pro+kilometrage_km+nb_portes, data_reg)
     return(summary(reg_sol))
@@ -82,89 +82,23 @@ mod_regression <- function(input, output, session, dataframe) {
     #Lets create the table we'll use later
     df_significant_coef <- tibble::rownames_to_column(as.data.frame(regression_function(df, choice)$coef), "coef")#Create a data frame with no row naes as it is easier to work without
 
+    keys <- data.frame(coef=c("energieDiesel","energieElectrique","kilometrage","energieEssence","energieHybride","energieGPL ou GNL","transmissionAutres","transmissionManuelle","transmissionSemi automatique","is_pro","nb_portes4","nb_portes5","nb_portes"), name_i=c("diesel car","electric car","car with + 10'000 km","gasoline car","hybrid car","GPL car","unvonventional transmission type car","manual transmission car","semi-atomatic transmission car","car from a profresional seller","car with 4 doors","car with 5 doors","nb_portes"))
 
+    # Take out intercept as it doesnt matter for our study
     df_significant_coef <- df_significant_coef %>%
-      dplyr::filter(coef!="(Intercept)") # Take out intercept as it doesnt matter for our study
-
-
-    #Long step, we change individually the name of oef for interpretation (couldn't do it in 1 mutatet as regression don't all have the same coefficients)
-    df_significant_coef  <- df_significant_coef %>%
-      dplyr::filter(coef== "energieElectrique") %>%
-      dplyr::mutate(coef="electric car") %>%
+      dplyr::filter(coef== "kilometrage_km") %>%  #Need to change kilometrage to have compute change for 10k
+      dplyr::mutate(Estimate=Estimate*10000, coef="kilometrage") %>%
       dplyr::bind_rows(df_significant_coef) %>%
-      dplyr::filter(coef != "energieElectrique")
+      dplyr::filter(coef != "kilometrage_km") %>%
 
-    df_significant_coef  <- df_significant_coef %>%
-      dplyr::filter(coef== "kilometrage_km") %>%
-      dplyr::mutate(Estimate=Estimate*10000, coef="car with + 10'000 km") %>%
-      dplyr::bind_rows(df_significant_coef) %>%
-      dplyr::filter(coef != "kilometrage_km")
-
-    df_significant_coef  <- df_significant_coef %>%
-      dplyr::filter(coef== "energieEssence") %>%
-      dplyr::mutate(coef="gasoline car") %>%
-      dplyr::bind_rows(df_significant_coef) %>%
-      dplyr::filter(coef != "energieEssence")
-
-    df_significant_coef  <- df_significant_coef %>%
-      dplyr::filter(coef== "energieHybride") %>%
-      dplyr::mutate(coef="hybrid car") %>%
-      dplyr::bind_rows(df_significant_coef) %>%
-      dplyr::filter(coef != "energieHybride")
-
-    df_significant_coef  <- df_significant_coef %>%
-      dplyr::filter(coef== "energieGPL ou GNL") %>%
-      dplyr::mutate(coef="GPL car") %>%
-      dplyr::bind_rows(df_significant_coef) %>%
-      dplyr::filter(coef != "energieGPL ou GNL")
-
-    df_significant_coef  <- df_significant_coef %>%
-      dplyr::filter(coef== "transmissionAutres") %>%
-      dplyr::mutate(coef="unvonventional transmission type car") %>%
-      dplyr::bind_rows(df_significant_coef) %>%
-      dplyr::filter(coef != "transmissionAutres")
-
-    df_significant_coef  <- df_significant_coef %>%
-      dplyr::filter(coef== "transmissionManuelle") %>%
-      dplyr::mutate(coef= "manual transmission car") %>%
-      dplyr::bind_rows(df_significant_coef) %>%
-      dplyr::filter(coef != "transmissionManuelle")
-
-    df_significant_coef  <- df_significant_coef %>%
-      dplyr::filter(coef== "transmissionSemi automatique") %>%
-      dplyr::mutate(coef= "semi-automatic transmission car") %>%
-      dplyr::bind_rows(df_significant_coef) %>%
-      dplyr::filter(coef != "transmissionSemi automatique")
-
-    df_significant_coef  <- df_significant_coef %>%
-      dplyr::filter(coef== "is_pro") %>%
-      dplyr::mutate(coef= "car from a profresional seller") %>%
-      dplyr::bind_rows(df_significant_coef) %>%
-      dplyr::filter(coef != "is_pro")
-
-    df_significant_coef  <- df_significant_coef %>%
-      dplyr::filter(coef== "nb_portes4") %>%
-      dplyr::mutate(coef= "car with 4 doors") %>%
-      dplyr::bind_rows(df_significant_coef) %>%
-      dplyr::filter(coef != "nb_portes4")
-
-    df_significant_coef  <- df_significant_coef %>%
-      dplyr::filter(coef== "nb_portes5") %>%
-      dplyr::mutate(coef= "car with 5 doors") %>%
-      dplyr::bind_rows(df_significant_coef) %>%
-      dplyr::filter(coef != "nb_portes5")
-
-    df_significant_coef  <- df_significant_coef %>%
-      dplyr::filter(coef== "nb_portes") %>%
-      dplyr::mutate(coef= "car with one more door") %>%
-      dplyr::bind_rows(df_significant_coef) %>%
-      dplyr::filter(coef != "nb_portes")
-
-
-    df_significant_coef  <- df_significant_coef %>%
+      dplyr::filter(coef!="(Intercept)") %>% # Take out intercept for interpretability
+      dplyr::left_join(keys, by="coef") %>% #join on coef to have interpretable name
+      dplyr::mutate(coef=as.character(name_i)) %>%
       dplyr::filter(`Pr(>|t|)`<0.05) %>% #Filter for significant features only
       dplyr::select(coef, Estimate)
 
+
+    #Long step, we change individually the name of oef for interpretation (couldn't do it in 1 mutatet as regression don't all have the same coefficients)
 
 
     #Split the significant coef into 2 separate table, the positive and the negative
@@ -173,9 +107,9 @@ mod_regression <- function(input, output, session, dataframe) {
     negativ_coef <- df_significant_coef %>% dplyr::filter(Estimate<0)
 
 
-    negativ_coef <- negativ_coef %>% purrr::pmap_chr(.f= function(Estimate,coef){paste("If you switch to a",coef,"you could save about", -signif(Estimate,2), "euros")})
+    negativ_coef <- negativ_coef %>% purrr::pmap_chr(.f= function(Estimate,coef){paste("If you switch to a",coef,"you could save about", -signif(Estimate,2), "€")})
 
-    positiv_coef <- positiv_coef %>% purrr::pmap_chr(.f= function(Estimate,coef){paste("If you switch to a", coef, "you would pay an extra", signif(Estimate,2), "euros")})
+    positiv_coef <- positiv_coef %>% purrr::pmap_chr(.f= function(Estimate,coef){paste("If you switch to a", coef, "you would pay an extra", signif(Estimate,2), "€")})
 
     return(c(negativ_coef,positiv_coef)) #Returns the two
 
